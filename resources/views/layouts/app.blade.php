1768bf9c8df545c389bd971dc106b897dc7d868f
Shahidul Islam <ishahin56@gmail.com>
2023-06-04 08:32:31 +0600

    Created dashboard structure and ngo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,23 +17,30 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <body x-data="data()" class="font-sans antialiased">
+        <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
+            <!-- Sidebar -->
+            @include('layouts.sidebar')
 
+            <div class="flex flex-col flex-1 w-full">
             <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+            @include('layouts.header')
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
+            <main class="h-full overflow-y-auto">
+                <div class="px-6 grid">
+                    @if (isset($header))
+                        <h2
+                        class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
+                        >
+                            {{ $header }}
+                        </h2>
+                    @endif
+                    <!-- Beginning contents -->
+                    {{ $slot }}
+                </div>
             </main>
+        </div>
         </div>
     </body>
 </html>
